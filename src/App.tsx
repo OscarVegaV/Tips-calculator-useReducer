97b5b35d2959b2aacf72a8b916fcb169a56f0a02
unknown <os_ar1@hotmail.com>
2025-06-06 18:03:30 -0600

    feat: add product list and initial bill section layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import MenuItem from "./componentes/MenuItem";
 import { menuItems } from "./data/db"
 
 function App() {
@@ -10,6 +11,28 @@ function App() {
       <header className=" bg-teal-400 py-5">
         <h1 className=" text-center text-4xl font-black"> Tip and Bill Calculator </h1>
       </header>
+
+      <main className=" max-w-7xl mx-auto py-20 grid md:grid-cols-2">
+
+        <div className="p-5">
+          <h2 className=" text-4xl font-black">Menu</h2>
+          
+          <div className=" space-y-3 mt-10">
+            {menuItems.map(item => (
+            <MenuItem
+              key={item.id}
+              item={item}
+            />
+
+          ))}
+          </div>
+        </div>
+
+        <div>
+          <h2>Bill</h2>
+        </div>
+
+      </main>
       
 
     </>
