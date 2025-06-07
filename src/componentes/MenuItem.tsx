e77d9f0dc322088ad0a968b045a39325fbf08759
unknown <os_ar1@hotmail.com>
2025-06-06 19:36:41 -0600

    feat: add order hook with function to add items to order

--- src/componentes/MenuItem.tsx
+++ src/componentes/MenuItem.tsx
@@ -2,12 +2,14 @@ import type { MenuItem } from "../types"
 
 type MenuItemProps = {
     item: MenuItem
+    addItem: () => void
 }
 
-export default function MenuItem({item} : MenuItemProps) {
+export default function MenuItem({item, addItem} : MenuItemProps) {
   return (
     <button 
-        className=" border-2 hover:bg-teal-200 border-teal-400 w-full p-3 flex justify-between"
+        className=" border-2 border-teal-400 hover:bg-teal-200 p-3 text-lg rounded-lg   flex justify-between w-full cursor-pointer "
+        onClick={() => addItem()}
     >
         <p>{item.name}</p>
         <p className="font-black">${item.price}</p>
