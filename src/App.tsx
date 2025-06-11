ed0393571e58676c26af4b3e71202e273bfa83d6
unknown <os_ar1@hotmail.com>
2025-06-10 21:53:04 -0600

    feat: added logic to calculate tip and total payable amount

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import MenuItem from "./components/MenuItem";
 import OrderContents from "./components/OrderContensts";
 import OrderTotals from "./components/OrderTotals";
-
+import TipPercentageForm from "./components/TipPercentageForm";
 import { menuItems } from "./data/db"
 import useOrder from "./hooks/useOrder";
 
 function App() {
   
-  const { order, addItem, removeItem } = useOrder()
+  const { order, addItem, removeItem, tip, setTip } = useOrder()
   
   return (
     <>
@@ -18,6 +18,7 @@ function App() {
       <main className=" max-w-7xl mx-auto py-20 grid md:grid-cols-2">
 
         <div className="p-5">
+
           <h2 className=" text-4xl font-black">Menu</h2>
           
           <div className=" space-y-3 mt-10">
@@ -35,12 +36,19 @@ function App() {
         <div className="border border-dashed border-slate-300 p-5 rounded-lg space-y-10">
 
           < OrderContents 
-            order={order}
-            removeItem={removeItem}
+              order={order}
+              removeItem={removeItem}
           />     
+
+          < TipPercentageForm 
+            
+            setTip={setTip}
+          
+          />
 
           <OrderTotals 
             order={order}
+            tip={tip}
           />
           
         </div>
--- src/components/OrderTotals.tsx
+++ src/components/OrderTotals.tsx
@@ -3,13 +3,20 @@ import type { OrderItem } from "../types"
 import { formatCurrency } from "../helpers"
 
 type OrderTotalsProps = {
-    order: OrderItem[]
+    order: OrderItem[],
+    tip: number
 }
 
 
-export default function OrderTotals({order} : OrderTotalsProps) {
+export default function OrderTotals({order, tip} : OrderTotalsProps) {
 
     const subtotalAmount = useMemo( () => order.reduce( (total,item) => total+ (item.quantity * item.price), 0 ) , [order] )
+
+    const tipAmount = useMemo( () => subtotalAmount * tip,  [tip, order] )
+
+    const totalAmount = useMemo( () => subtotalAmount + tipAmount, [tip, order] )
+
+
 
     return (
 
@@ -21,11 +28,11 @@ export default function OrderTotals({order} : OrderTotalsProps) {
             </p>
 
                 <p>Service Charge: {' '}
-                <span className="font-bold"> $0</span>
+                <span className="font-bold">{formatCurrency(tipAmount)}</span>
             </p>
 
                 <p>Grand Total: {' '}
-                <span className="font-bold"> $0</span>
+                <span className="font-bold">{formatCurrency(totalAmount)}</span>
             </p>
             
         </div>
